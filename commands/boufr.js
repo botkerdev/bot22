a0cd4db2946cc1ab115d424018905f68c4a1fe87
botkerdev <122660103+botkerdev@users.noreply.github.com>
2023-03-19 14:30:26 +0900

    Update and rename boufr.js to 남친생성.js

--- commands/boufr.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: "남친생성",
-    execute(message){
-        message.channel.send(`error = 'boyfriend' NOT FOUND\nerror = Not found data => 'boyfriend_summon' error_message => 니 면상이나 쳐 보세요`)
-    }
-}
---
+++ commands/남친생성.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: "남친생성",
+    execute(message){
+        message.channel.send(`error = 'boyfriend' NOT FOUND\nerror = Not found data => 'boyfriend_summon' error_message => Error`)
+    }
+}
