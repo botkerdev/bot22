a3abf96e53255b541846734c2d1c6ae2500614f6
botkerdev <122660103+botkerdev@users.noreply.github.com>
2023-03-19 14:34:11 +0900

    Update and rename girlfr.js to 여친생성.js

--- commands/girlfr.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: "여친생성",
-    execute(message){
-        message.channel.send(`error = 'girlfriend' NOT FOUND\nerror = Not found data => 'girlfriend_summon' error_message => 니 면상이나 쳐 보세요`)
-    }
-}
---
+++ commands/여친생성.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: "여친생성",
+    execute(message){
+        message.channel.send(`error = 'girlfriend' NOT FOUND\nerror = Not found data => 'girlfriend_summon' error_message => Error`)
+    }
+}
